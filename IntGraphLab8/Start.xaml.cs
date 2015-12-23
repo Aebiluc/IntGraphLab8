f358f6b98f3eef0ac17269d94d0adbf51588bc85
Luc Aebischer <aebiluc@gmail.com>
2015-12-23 22:10:38 +0100

    Séléction des utilisateurs visible. (Changement de couleur de contour)

--- IntGraphLab8/Start.xaml.cs
+++ IntGraphLab8/Start.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace IntGraphLab8
 {
@@ -14,17 +15,25 @@ namespace IntGraphLab8
         private void ButtonOperator_Click(object sender, RoutedEventArgs e)
         {
             SelectedUser.UserStatus = UserType.Operator;
-            
+            buttonOperateur.Background = Brushes.DarkGray;
+            ButtonManager.Background = Brushes.LightGray;
+            ButtonAdmin.Background = Brushes.LightGray;
         }
 
         private void ButtonManager_Click(object sender, RoutedEventArgs e)
         {
             SelectedUser.UserStatus = UserType.Manager;
+            ButtonManager.Background = Brushes.DarkGray;
+            buttonOperateur.Background = Brushes.LightGray;
+            ButtonAdmin.Background = Brushes.LightGray;
         }
 
         private void ButtonAdmin_Click(object sender, RoutedEventArgs e)
         {
             SelectedUser.UserStatus = UserType.Admin;
+            ButtonAdmin.Background = Brushes.DarkGray;
+            buttonOperateur.Background = Brushes.LightGray;
+            ButtonManager.Background = Brushes.LightGray;
         }
 
         public User SelectedUser { get; set; }
